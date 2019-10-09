2b6d6879eaafc0c11890257f94ce17872300974b
Raul Ramirez <raul.ramirez@escuelaestech.es>
2019-10-09 15:52:07 +0200

    Ejercicio 5

--- Ejercicio_5/commun.inc.php
+++ Ejercicio_5/commun.inc.php
@@ -1,9 +1,7 @@
 <?php
-    define('MI_SITIO', 'miSitio.com');
-    $autores = ['Victor Hugo', 'Charles Baudelaire', 'Arthur Rimbaud', 'Paul Verlaine'];
-    function analizar_vocales($nombre, &$numero, &$empiza_por){
+    function analizar_vocales($nombre, &$numero, &$empieza_por){
         $vocales = ['a', 'e', 'i', 'o', 'u'];
-        $numero = strlen($nomnre);
-        $empiza_por = in_array(strtolower($nombre[0]), $vocales);
+        $numero = strlen($nombre);
+        $empieza_por = in_array(strtolower($nombre[0]), $vocales);
     }
 ?>
